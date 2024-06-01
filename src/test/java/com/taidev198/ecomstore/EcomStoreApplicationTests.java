5c7f54fd1eb609645839159c4660253e1835cff3
traigiuxom9x@gmail.com <traigiuxom9x@gmail.com>
2024-06-01 09:49:56 +0700

    Initial commit

---
+++ src/test/java/com/taidev198/ecomstore/EcomStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taidev198.ecomstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
